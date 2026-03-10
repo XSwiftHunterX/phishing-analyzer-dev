a87f1ab792ab5f10e1325e513574d19d59920ebd
Ubuntu <ubuntu@ip-172-31-11-15.us-east-2.compute.internal>
2026-03-10 18:23:54 +0000

    Added search and filtering for messages

--- analyzer/views.py
+++ analyzer/views.py
@@ -6,7 +6,33 @@ from .forms import MessageForm
 
 def message_list(request):
     messages = Message.objects.all().order_by('-submission_date')
-    return render(request, 'analyzer/message_list.html', {'messages': messages})
+
+    query = request.GET.get('q')
+    classification = request.GET.get('classification')
+    risk_level = request.GET.get('risk_level')
+    message_type = request.GET.get('message_type')
+
+    if query:
+        messages = messages.filter(message_content__icontains=query)
+
+    if classification:
+        messages = messages.filter(classification=classification)
+
+    if risk_level:
+        messages = messages.filter(risk_level=risk_level)
+
+    if message_type:
+        messages = messages.filter(message_type=message_type)
+
+    context = {
+        'messages': messages,
+        'query': query or '',
+        'selected_classification': classification or '',
+        'selected_risk_level': risk_level or '',
+        'selected_message_type': message_type or '',
+    }
+
+    return render(request, 'analyzer/message_list.html', context)
 
 def submit_message(request):
     if request.method == 'POST':
