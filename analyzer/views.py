2a09b96df8dd0074eb49b9233a6a7d3c7a02c118
Ubuntu <ubuntu@ip-172-31-11-15.us-east-2.compute.internal>
2026-03-10 07:19:38 +0000

    Added message list to display all stored messages

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Message
 
 # Create your views here.
+
+def message_list(request):
+    messages = Message.objects.all().order_by('-submission_date')
+    return render(request, 'analyzer/message_list.html', {'messages': messages})
