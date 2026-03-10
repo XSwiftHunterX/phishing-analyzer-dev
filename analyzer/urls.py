64aeb55096fa75feb40519587ba606657defaf7a
Ubuntu <ubuntu@ip-172-31-11-15.us-east-2.compute.internal>
2026-03-10 07:33:32 +0000

    Added message submission form

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.message_list, name='message_list'),
+    path('submit/', views.submit_message, name='submit_message'),
 ]
--- analyzer/views.py
+++ analyzer/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Message
+from .forms import MessageForm
 
 # Create your views here.
 
 def message_list(request):
     messages = Message.objects.all().order_by('-submission_date')
     return render(request, 'analyzer/message_list.html', {'messages': messages})
+
+def submit_message(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('message_list')
+    else:
+        form = MessageForm()
+
+    return render(request, 'analyzer/submit_message.html', {'form': form})
